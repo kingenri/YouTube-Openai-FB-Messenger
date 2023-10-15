76f2d23cf30cf8ada0055e10bed2f91d105a830e
kingenri <35837613+kingenri@users.noreply.github.com>
2023-10-15 15:24:55 +0800

    Update fbWebhookRoute.js

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -14,8 +14,12 @@ router.get('/', (req, res) => {
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     } else {
+      console.error('Failed to verify webhook');
       res.sendStatus(403);
     }
+  } else {
+    console.error('Invalid request');
+    res.sendStatus(400);
   }
 });
 
@@ -23,14 +27,26 @@ router.post('/', async (req, res) => {
   try {
     let body = req.body;
     let requestType = body.object;
-    let senderId = body.entry[0].messaging[0].sender.id;
-    let query = body.entry[0].messaging[0].message.text;
-    let result = await chatCompletion(query);
-    await sendMessage(senderId, result.response);
+    if (requestType === 'page' && body.entry && body.entry[0] && body.entry[0].messaging && body.entry[0].messaging[0]) {
+      let senderId = body.entry[0].messaging[0].sender.id;
+      let query = body.entry[0].messaging[0].message.text;
+      if (query) {
+        let result = await chatCompletion(query);
+        if (result && result.response) {
+          await sendMessage(senderId, result.response);
+        } else {
+          console.error('Failed to get response from OpenAI');
+        }
+      } else {
+        console.error('Empty message received');
+      }
+    } else {
+      console.error('Invalid request format');
+    }
   } catch (error) {
-    console.log(error);
+    console.error('Error occurred:', error);
   }
-  res.status(200).send('OK');
+  res.status(200).send('EVENT_RECEIVED');
 });
 
 module.exports = {
